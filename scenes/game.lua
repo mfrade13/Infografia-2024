741711c1d5bd99a5c83a9509700f55729cad9476
Giuliana Defilippis <giulianadefilippisv1@upb.edu>
2024-02-21 12:05:03 -0400

    Added bombs. When the user hits a bomb, the gameover scene is displayed. Added three lifes to the user. A life is lost if the user doesn't hit a fruit and this one has already disappeared.

--- scenes/game.lua
+++ scenes/game.lua
@@ -2,6 +2,7 @@ local composer = require( "composer" )
  
 local scene = composer.newScene()
 local background
+local grpMain = display.newGroup()
 
 -- Variables
 
@@ -15,6 +16,8 @@ local fruitNames = {
 local fruits = {}
 local trail = {}
 local score = 0
+local lifes = 3
+local katanas = {}
 local numberFrames = 0
 
 local scoreLabel
@@ -32,26 +35,32 @@ local yVelocities = {
     -7,
 }
 
+function endGame()
+    composer.gotoScene("scenes.gameover", { effect = "fade", time = 1000 })
+end
 
-local function createTrail(event)
+function createTrail(event)
 
     if event.phase == "moved" then
         local x = event.x
         local y = event.y
-        local dot = display.newCircle(x, y, 10)
+        local dot = display.newCircle(grpMain, x, y, 10)
         dot:setFillColor(1,1,1)
         dot.alpha = 0.6
         table.insert(trail,dot)
 
         for i = 1, #fruits, 1 do
             local fruit = fruits[i][1]
-            if ((event.x >= fruit.contentBounds.xMin) and (event.x <= fruit.contentBounds.xMax) and
-                (event.y >= fruit.contentBounds.yMin) and (event.y <= fruit.contentBounds.yMax)) == true
-                and fruits[i][4] == true then
+
+            if (event.x >= fruit.contentBounds.xMin) and (event.x <= fruit.contentBounds.xMax) and
+                (event.y >= fruit.contentBounds.yMin) and (event.y <= fruit.contentBounds.yMax) and fruits[i][7] then
                 score = score + 10
-                print("SCOREEE: " ..score)
+                
+                if (fruit.tag == "bomb") then
+                    endGame()
+                end
                 fruit.isVisible =false
-                fruits[i][4] = false
+                fruits[i][7] = false
                 scoreLabel.text = score .. "p"
 
             end
@@ -60,7 +69,7 @@ local function createTrail(event)
     end
 end
 
-local function updateTrail()
+function updateTrail()
     for i = #trail, 1, -1 do
         local dot = trail[i]
         dot.alpha = dot.alpha - 0.02
@@ -78,38 +87,87 @@ end
 --
 -- Add Fruits
 
-local function addFruit()
+function updateFruitPosition(fruit, xo, yo, velocity, angle, t)
+    local gravity = 0.04
 
+    fruit.x = xo + velocity * math.cos(angle) * t
+    fruit.y = yo -  velocity * math.sin(angle) * t + 0.5 * gravity * t^2
+end
+
+function addFruit(isBomb)
+    local isBomb = isBomb or nil
+
+    if isBomb then 
+        local bomb = display.newImageRect(grpMain,'assets/bomb.png', 90,120 )
+        
+        bomb.x = math.random(200, CW-200)
+        bomb.y = CH
+        bomb.tag = "bomb"
+        local velocity = math.random(7,12)
+        local angle = math.random(math.pi/3, math.pi-math.pi/3)
+        local xo = bomb.x
+        local yo = bomb.y
+        local time = 0
+        fruits[#fruits + 1] = {bomb, xo, yo, velocity, angle, time, true}
+    end
     for i = #fruitNames, 1, -1 do
         local fruit       
         rand = math.random(1,#fruitNames)
         if rand == 3 then --pineapple           
-            fruit = display.newImageRect(fruitNames[rand], 150,180)
+            fruit = display.newImageRect(grpMain, fruitNames[rand], 150,180)
         else
-            fruit = display.newImageRect(fruitNames[rand], 120,120)
+            fruit = display.newImageRect(grpMain, fruitNames[rand], 120,120)
         end
         
-        fruit.anchorX = 0
-        fruit.anchorY = 0
-        fruit.x = math.random(200, CW)
+
+        fruit.x = math.random(200, CW-200)
         fruit.y = CH
-        local xVelocity =xVelocities[math.random (1, #xVelocities)]
-        local yVelocity = yVelocities[math.random (1, #yVelocities)]
-        fruits[#fruits + 1] = {fruit, xVelocity, yVelocity, true}
+        local velocity = math.random(5,9)
+        local angle = math.random(math.pi/3, math.pi-math.pi*2/3)
+        local xo = fruit.x
+        local yo = fruit.y
+        local time = 0
+        fruits[#fruits + 1] = {fruit, xo, yo, velocity, angle, time, true}
     end   
 end
 
-local function manageFruits()
+function manageFruits()
     for i = #fruits, 1, -1 do
         local localFruit = fruits[i][1]
-        localFruit:translate(fruits[i][2], fruits[i][3])
+        fruits[i][6] = fruits[i][6] + 1
 
-        if localFruit.y <= CH/5 then
-            fruits[i][3] = - fruits[i][3]
+        updateFruitPosition(fruits[i][1],fruits[i][2], fruits[i][3], fruits[i][4], fruits[i][5], fruits[i][6])
+
+        if localFruit.x <= 0 then -- left collision
+            fruits[i][2] = fruits[i][1].x -- xo
+            fruits[i][3] = fruits[i][1].y -- yo
+            fruits[i][4] = - (fruits[i][4] + 2)-- velocity
+            fruits[i][5] = fruits[i][5] + math.pi/4
+            fruits[i][6] = 0
+        elseif localFruit.x >= CW  then -- right collision
+            fruits[i][2] = fruits[i][1].x -- xo
+            fruits[i][3] = fruits[i][1].y -- yo
+            fruits[i][5] = fruits[i][5] + math.pi * 9/10
+            fruits[i][6] = 0
+        elseif localFruit.y <= 0 then -- top collision
+            fruits[i][2] = fruits[i][1].x
+            fruits[i][3] = fruits[i][1].y
+            fruits[i][4] = - fruits[i][4]
+            fruits[i][5] = fruits[i][5] + math.pi/9
+            fruits[i][6] = 0
         end
 
-        if localFruit.x <-80 or localFruit.y < -80 or localFruit.x > CW + 80
-            or localFruit.y > CH +80 or fruits[i][4] == false then
+        if localFruit.y > CH + 80 or fruits[i][7] == false then
+
+            if (localFruit.y > CH + 80 and fruits[i][7] == true and lifes > 0 and localFruit.tag  ~= "bomb") then
+                local katana = katanas[#katanas]
+                table.remove(katanas, #katanas)
+                katana:removeSelf()
+                katana = nil
+                fruits[i][7] = false
+                lifes = lifes - 1
+            end
+
             local b = table.remove(fruits, i)
             if b ~= nil then
                 b[1]:removeSelf()
@@ -118,17 +176,25 @@ local function manageFruits()
         end       
     end
 
-    if numberFrames > 150 then
+    if numberFrames > 200 then
         numberFrames = 0
         addFruit()
+    elseif numberFrames == 150 then
+        addFruit(true)
     end
 
     numberFrames = numberFrames + 1
 end
 
-local function update()
+
+
+function update()
     updateTrail()
     manageFruits()
+    
+    if (lifes == 0 ) then
+        endGame()
+    end
 end
 
 -- -----------------------------------------------------------------------------------
@@ -138,13 +204,22 @@ end
 -- create()
 function scene:create( event )
     local sceneGroup = self.view
-    background = display.newImageRect(sceneGroup, 'assets/background.png', CW, CH)
+
+    sceneGroup:insert(grpMain)
+    background = display.newImageRect(grpMain, 'assets/background.png', CW, CH)
     background.x = HW; background.y = HH
+    
 
-    scoreLabel = display.newText(sceneGroup, score .. "p", CW/7, CH/9, "PressStart2P-Regular.ttf", CH/20)
+    for i=1, lifes, 1 do
+        local katana = display.newImageRect(grpMain,    'assets/katana.png', 100, 100)
+        katana.y = CH/9.8; katana .x = 5*CW/7 +  (60 * i)
+        katana.tag = i
+        table.insert(katanas, katana)
+    end
 
-    Runtime:addEventListener("enterFrame", update)
-    Runtime:addEventListener("touch", createTrail)
+    scoreLabel = display.newText(grpMain, score .. "p", CW/7, CH/9, "PressStart2P-Regular.ttf", CH/20)
+
+
 end
  
  
@@ -155,10 +230,12 @@ function scene:show( event )
     local phase = event.phase
  
     if ( phase == "will" ) then
-        -- Code here runs when the scene is still off screen (but is about to come on screen)
+        Runtime:addEventListener("enterFrame", update)
+        Runtime:addEventListener("touch", createTrail)
+        lifes = 3
  
     elseif ( phase == "did" ) then
-        -- Code here runs when the scene is entirely on screen
+        
     end
 end
  
@@ -170,26 +247,26 @@ function scene:hide( event )
     local phase = event.phase
  
     if ( phase == "will" ) then
-        -- Code here runs when the scene is on screen (but is about to go off screen)
- 
+        Runtime:removeEventListener("enterFrame", update)
+        Runtime:removeEventListener("touch", createTrail)
+        composer.removeScene("scenes.game")
+        
     elseif ( phase == "did" ) then
-        -- Code here runs immediately after the scene goes entirely off screen
- 
+        
+        
+        
     end
 end
  
  
 -- destroy()
 function scene:destroy( event )
- 
-    local sceneGroup = self.view
-    -- Code here runs prior to the removal of scene's view
- 
+    local sceneGroup = self.view    
 end
  
  
 -- -----------------------------------------------------------------------------------
--- Scene event function listeners
+-- Scene event function li histeners
 -- -----------------------------------------------------------------------------------
 scene:addEventListener( "create", scene )
 scene:addEventListener( "show", scene )
--- scenes/gameover.lua
+++ scenes/gameover.lua
@@ -1,7 +1,7 @@
 local composer = require( "composer" )
  
 local scene = composer.newScene()
-local background
+local background, message, restart
 
 -- -----------------------------------------------------------------------------------
 -- Scene event functions
@@ -10,9 +10,18 @@ local background
 -- create()
 function scene:create( event )
     local sceneGroup = self.view
-    background = display.newImageRect('assets/background.png', CW, CH)
+    background = display.newImageRect(sceneGroup, 'assets/gameover_background.jpg', CW, CH)
     background.x = HW; background.y = HH
-    -- Code here runs when the scene is first created but has not yet appeared on screen
+    background.alpha = 0.7
+    
+    message = display.newText(sceneGroup, "Game Over", HW, HH, "PressStart2P-Regular.ttf", CH/12)
+
+    restart = display.newImageRect(sceneGroup, 'assets/restart.png',CW/12, CW/12)
+    restart.x = HW; restart.y = HH + CH/5
+
+    restart:addEventListener("touch", function ()
+        composer.gotoScene('scenes.menu',{ effect = "fade", time = 1000 })
+    end)
  
 end
  
@@ -43,15 +52,13 @@ function scene:hide( event )
         -- Code here runs when the scene is on screen (but is about to go off screen)
  
     elseif ( phase == "did" ) then
-        -- Code here runs immediately after the scene goes entirely off screen
- 
+
     end
 end
  
  
 -- destroy()
 function scene:destroy( event )
- 
     local sceneGroup = self.view
     -- Code here runs prior to the removal of scene's view
  
