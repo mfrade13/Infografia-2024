f7262ab9da3d5aefbd01a2fa54c5ea91dce5654c
mfrade13 <mfrade13@isc.lp.upb.edu>
2024-02-28 21:43:13 -0400

    Adicion de la camara.

--- Proyecto_Animacion/main.lua
+++ Proyecto_Animacion/main.lua
@@ -13,14 +13,21 @@ local path = "resources/"
 physics.start( )
 physics.setDrawMode( "hybrid" )
 
+local grupoFondo = display.newGroup()
+local grupoPersonaje = display.newGroup()
+local grupoInterfaz =  display.newGroup()
+
+
 print(physics.getGravity())
 physics.setGravity( 0, 9.8 )
 
 
 --local fondo = display.newRect(CW/2, CH/2, CW, CH)
-local fondo = display.newImageRect(path.."1.jpg", CW, CH)
+local fondo = display.newImageRect(grupoFondo, path.."1.jpg", CW, CH)
 --fondo:setFillColor(0.22)
-fondo.x = CW/2; fondo.y = CH/2
+fondo.x = 0; fondo.y = CH/2
+local fondo2 = display.newImageRect(grupoFondo, path.."1.jpg", CW, CH)
+fondo2.x = CW; fondo2.y = CH/2
 
 local options = {
     width = 300,
@@ -75,7 +82,7 @@ local secuencia = {
 local cuerpo_box_cazador = { halfWidth=55, halfHeight=100, x=0, y=0, angle=0 }
 
 
-local cazador = display.newSprite(image_sheet_acciones_d, secuencia)
+local cazador = display.newSprite(grupoPersonaje, image_sheet_acciones_d, secuencia)
 cazador.x = CW/2; cazador.y = CH/2
 cazador.nombre = "cazador"
 print(cazador.xScale, cazador.yScale)
@@ -84,12 +91,15 @@ physics.addBody( cazador, "dynamic", {box=cuerpo_box_cazador, bounce = 0.5,densi
 print(cazador.bodyType)
 --physics.addBody(cazador, "dynamic", {radius = 85})
 
-local piso = display.newRect(CW/2, CH-150, CW, 30)
-piso:setFillColor( 0,1,0, 0.4 )
+local piso = display.newRect(grupoPersonaje, CW/2, CH-150, CW, 30)
+piso:setFillColor( 1,1,0, 0.4 )
 piso.nombre = "piso"
 physics.addBody(piso, "static", {friccion=0.2})
 
-local ball = display.newCircle(CW/3,CH/2, 50)
+local piso2 = display.newRect(grupoPersonaje, CW , CH-100, CW, 30)
+physics.addBody(piso2, "static", {friccion=0.2})
+piso2.nombre = "piso"
+local ball = display.newCircle(grupoPersonaje,   CW/3,CH/2, 50)
 ball:setFillColor( 0,0,1 )
 ball.nombre = "ball"
 physics.addBody(ball, "kinematic", {radius=50, bounce = 1, isSensor = true})
@@ -105,7 +115,7 @@ function mover(e)
         print("Empieza a avanzar")
         cazador:setSequence("avanza_derecha")
         local direccion = e.target.direccion
-        local fuerzaX = e.target.fuerza
+        local fuerzaX = e.target.fuerza * 11
         --Runtime verificar boton apretado para avanzar con su posicion en X
         if direccion == "derecha" then
             cazador.xScale =  1
@@ -114,15 +124,20 @@ function mover(e)
             cazador.xScale =  -1
             xTarget = 0
         end
-        cazador:applyLinearImpulse(fuerzaX, 0, cazador.x, cazador.y)
         print("Masa cazador: ",cazador.mass)
+        cazador:setLinearVelocity(fuerzaX, 0)
+        --grupoFondo.x = - cazador.x +CW/2
+        --transition.to(grupoFondo, {x = xTarget, time=1000})
+        --cazador:applyLinearImpulse(fuerzaX, 0, cazador.x, cazador.y)
  --       cazador:applyForce(fuerzaX,0,cazador.x, cazador.y)
---transition.to(cazador, {x=xTarget, time=3000})
+        --transition.to(cazador, {x=xTarget, time=3000})
         cazador:play()
         --cazador:translate(10,0)
     elseif e.phase == "ended" or e.phase == "cancelled" then
-        print("se detiene")
+        print("se detiene, posicion en X:", cazador.x)
         transition.cancel(cazador)
+        transition.cancel(grupoFondo)
+        cazador:setLinearVelocity(0, 0)
         cazador:setSequence("reposo_derecha")
         cazador:play()
     end
@@ -132,7 +147,7 @@ end
 function cazador_collision(self, event)
     if event.phase == "began" then
         if event.other.nombre == "ball" then
-            display.remove(event.other)
+            --display.remove(event.other)
             print("Sumar puntos")
         end
     elseif event.phase == "ended" then
@@ -147,18 +162,23 @@ cazador:addEventListener( "collision", cazador )
 
 local boton_avanzar_derecha = display.newRect(CW-100, CH-100, 100, 70)
 boton_avanzar_derecha.direccion = "derecha"
-boton_avanzar_derecha.fuerza = 10
+boton_avanzar_derecha.fuerza = 15
 boton_avanzar_derecha:addEventListener("touch", mover)
 
 local boton_avanzar_izquierda = display.newRect(100, CH-100, 100, 70)
 boton_avanzar_izquierda.direccion = "izquierda"
-boton_avanzar_izquierda.fuerza = -10
+boton_avanzar_izquierda.fuerza = -15
 boton_avanzar_izquierda:addEventListener("touch", mover)
 
 local boton_frames = display.newRect(CW/2, boton_avanzar_derecha.y, 100,70)
 boton_frames:addEventListener("touch", function()
     cazador:setSequence("atacar")
     cazador:play()
+    cazador:applyLinearImpulse(0, -10,cazador.x, cazador.y)
+    ball.bodyType = "dynamic"
+    ball.isSensor = false
+    ball:applyForce(0, 0.2, ball.x, ball.y)
+
     cazador.bodyType = "dynamic"
     timer.performWithDelay( 1200, function()
         cazador:setSequence( "reposo_derecha" )
@@ -168,4 +188,14 @@ boton_frames:addEventListener("touch", function()
     -- cazador:pause()
 end)
 
+
+function camara(e)
+    if cazador.x >CW/2 then
+        grupoFondo.x = -cazador.x + CW
+    end
+    grupoPersonaje.x = -cazador.x + CW/2
+    --grupoFondo.y = cazador.y - CH/2
+end
+
+Runtime:addEventListener("enterFrame", camara)
 
